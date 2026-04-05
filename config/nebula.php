f36b7df853477d53bc142bb404cb74f7709b7af4
sonarta <firdausriawan2@gmail.com>
2026-04-05 21:16:52 +0700

    fix: set CMS version to 1.0.2 in config

--- config/nebula.php
+++ config/nebula.php
@@ -11,7 +11,7 @@ return [
     | to compare against the latest GitHub release.
     |
     */
-    'version' => '1.0.0',
+    'version' => '1.0.2',
 
     /*
     |--------------------------------------------------------------------------
